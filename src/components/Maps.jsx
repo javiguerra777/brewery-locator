2c81fbf7696c7d8683e03df728879c5804a17622
srirachy <caseyereyes@gmail.com>
2022-05-03 14:59:07 -0700

    feat(maps) add remaining functionality to map component

--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
 import { Map, Marker, Overlay } from 'pigeon-maps';
 import { maptiler } from 'pigeon-maps/providers';
-import RenderOverlays from './RenderOverlays';
-import beerImg from '../img/beer.png';
 import styled from 'styled-components';
+import beerImg from '../img/beer.png';
+import {nanoid} from 'nanoid';
 
 
 const apiKey = process.env.REACT_APP_API_KEY;
-// const apiKey = 'xjvsbhH2HT83hJjmdqQ2'
-//const apiKey = process.env.REACT_APP_API_KEY;
 const maptilerProvider = maptiler(apiKey, 'streets');
 
-const meow = () => {
-    console.log('meow');
+const meow = (bname, blng, blat) => {
+    console.log(bname);
+    console.log(blng);
+    console.log(blat);
 }
 
 const ChangeMouse = styled.div`
@@ -22,30 +23,60 @@ const ChangeMouse = styled.div`
 `
 
 //console.log(apiKey)
-const Maps = ( {data} ) => {
+const Maps = ( {data, lng, lat} ) => {
+    const [curLng, setCurLng] = useState(0);
+    const [curLat, setCurLat] = useState(0);
+    const [lngData, setLngData] = useState([]);
+    const [latData, setLatData] = useState([]);
+
+    useEffect(() => {
+        //set longitude and latitude array
+        setLngData(data.map(brewery => brewery.longitude))
+        setLatData(data.map(brewery => brewery.latitude))
+        //set default center
+        const fcLng=parseFloat(lngData[0]);
+        const fcLat=parseFloat(latData[0]);
+        setCurLng(fcLng);
+        setCurLat(fcLat);
+    }, [data])
+
+    const renderOverlays = () => {
+        const coordElmts=[];
+
+        for (const {name, longitude, latitude} of data){
+            const flat=parseFloat(latitude)
+            const flng=parseFloat(longitude)
+            coordElmts.push(
+                <Overlay anchor={[flat, flng]} key={nanoid()}>
+                    <img src={beerImg} width={50} height={50} onClick={() => meow(name, longitude, latitude)} alt={name}/>
+                </Overlay>
+            )
+        }
+        //console.log(coordElmts);
+        return coordElmts;
+    }
+
+    const renderMap = () => {
+        const mapElmt=[];
+        
+        mapElmt.push(
+            <Map provider={maptilerProvider}
+                key={nanoid()}
+                dprs={[1,2]}
+                height={500}
+                defaultCenter={[lat, lng]}
+                defaultZoom={11}>
+                {renderOverlays()}
+            </Map>
+        );
+
+        return mapElmt;
+    };
 
     //console.log(`${apiKey}`)
     return (
         <ChangeMouse>
-        <Map
-            provider={maptilerProvider}
-            dprs={[1, 2]}   //setstate dprs
-            height={500}    //setstate height
-            defaultCenter={[50.879, 4.6997]} //setstate long, lat**
-            //setstate zoom 
-            defaultZoom={11}>
-            {/* <RenderMarkers></RenderMarkers> */}
-            {/* i think we'll decide between overlay and markers */}
-            {/* <Marker width={50} anchor={[50.879, 4.6997]} />
-            <Marker width={50} anchor={[50, 4]} onClick={() => meow()}/> */}
-            <RenderOverlays data={data}></RenderOverlays>
-            {/* <Overlay anchor={[50.879, 4.6997]} offset={[22, 60]}>
-                <img src={beerImg} width={50} height={50} onClick={() => meow()}  alt='' />
-            </Overlay>
-            <Overlay anchor={[50, 4]} offset={[22, 60]}>
-                <img src={beerImg} width={50} height={50} onClick={() => meow()}  alt='' />
-            </Overlay> */}
-        </Map>
+        {renderMap()}
         </ChangeMouse>
     )                                                                   
 }                                                                           
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,16 +2,16 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { nanoid } from 'nanoid';
 import Maps from '../components/Maps'
-import './mainpage.css';
+import '../components/mainpage.css';
 
 const Main = () => {
   const [location, setLocation] = useState('Fresno, CA');
   const fixedLocation = location.split(',');
   const [newLocation, setNewLocation] = useState('');
   const [data, setData] = useState([]);
+  const [lng, setLng] = useState(0);
+  const [lat, setLat] = useState(0);
   const breweryUrl = `https://api.openbrewerydb.org/breweries?by_city=${fixedLocation[0]}`;
-  //const [breweries, setBreweries] = useState(data.map((brewery)=> brewery.name));
-  //console.log('Here are the breweries', breweries)
   const handleLocationChange = (e) => {
     setNewLocation(e.target.value);
   }
@@ -28,9 +28,28 @@ const Main = () => {
     .catch((err)=> console.log(err));
 
   }, [location]);
+  //use effect for default center in map component
+  useEffect(() => {
+    if(data.length){
+      //for (const {name, longitude, latitude} of data){
+      let foundLng = '';
+      const allData = data.entries();
+      //loop until find a longitude
+      while (foundLng === ''){
+        let nextData = allData.next().value;
+        let tempLng = parseFloat(nextData[1].longitude);
+        let tempLat = parseFloat(nextData[1].latitude);
+        if (!isNaN(tempLng)){
+          foundLng = tempLng;
+          setLng(tempLng);
+          setLat(tempLat);
+        }
+      }
+    }
+  }, [data, location])
   return (
     <>
-    <Maps data={data}></Maps>
+    {(data && lng && lat) && <Maps data={data} lng={lng} lat={lat}></Maps>}
     <div className='container'>
       <div className='header'>
       <h1>Brewery Search For <strong>{location}</strong>:</h1>
